07088dfd9656844b218558d4e1505fbdcc47b76c
IDM014 <IDM014@IDM012s-Mac-mini.local>
2018-12-13 14:56:02 +0530

    changes done

---
+++ Loco/Loco/Loco.h
@@ -0,0 +1,19 @@
+//
+//  Loco.h
+//  Loco
+//
+//  Created by IDM014 on 13/12/18.
+//  Copyright © 2018 e. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Loco/MethodViewController.h>
+//! Project version number for Loco.
+FOUNDATION_EXPORT double LocoVersionNumber;
+
+//! Project version string for Loco.
+FOUNDATION_EXPORT const unsigned char LocoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Loco/PublicHeader.h>
+
+
---
+++ Loco/MethodViewController.m
@@ -0,0 +1,26 @@
+//
+//  MethodViewController.m
+//  Loco
+//
+//  Created by IDM014 on 13/12/18.
+//  Copyright © 2018 e. All rights reserved.
+//
+
+#import "MethodViewController.h"
+
+@interface MethodViewController ()
+
+@end
+
+@implementation MethodViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+-(void)viewMessage{
+    NSLog(@"::::SDK FRAMEWORK");
+}
+
+@end
